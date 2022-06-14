2e34dd56697547361069e77c3bd9df04bcd84ef4
Bagas Tri Wibowo <bagas.tri@tanihub.com>
2022-06-14 09:11:01 +0700

    [ADD] constant wesata icon list

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -12,31 +12,6 @@ class Owner extends Model
     use HasApiTokens;
     protected $fillable = ['name', 'email', 'password', 'birthday', 'phone_number', 'shop', 'shop_img_url'];
 
-    public static $listIconUrl = [
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/wave_lu8oml.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/volleyball_qu3nea.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/toy_jvpp3d.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun-hat_zgiset.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sunflower_bg8l7h.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/surfboard_aem0v8.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun-umbrella_sklizh.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun_m50qxi.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/suitcase_iqsjf2.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/kayak_hipsbf.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/sandal_m3y4cy.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/sand-castle_c1ru1b.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/deck-chair_hlpbeq.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/diving-glasses_blowwb.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/honeycomb_wucbxb.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/footprint_lhzpw1.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/coral_vvpohj.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/coconut-tree_qaf8ae.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/cherry_u5ima3.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/camping_maxzbe.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/balloons_fuo1xa.png',
-        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/bee_qswc8z.png'
-    ];
-
     public function cashiers() {
         return $this->hasMany(Cashier::class);
     }
@@ -56,9 +31,10 @@ class Owner extends Model
 
     public static function getRandomIcon()
     {
-        $size = sizeof(Owner::$listIconUrl);
+        $iconList = config('constants.wesata_shop_icon_list');
+        $size = sizeof($iconList);
         $randomIndex = random_int(0, $size - 1);
 
-        return Owner::$listIconUrl[$randomIndex];
+        return $iconList[$randomIndex];
     }
 }
---
+++ config/constants.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'wesata_shop_icon_list' => [
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/wave_lu8oml.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/volleyball_qu3nea.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130982/shopWesataIcon/toy_jvpp3d.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun-hat_zgiset.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sunflower_bg8l7h.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/surfboard_aem0v8.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun-umbrella_sklizh.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/sun_m50qxi.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130981/shopWesataIcon/suitcase_iqsjf2.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/kayak_hipsbf.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/sandal_m3y4cy.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/sand-castle_c1ru1b.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/deck-chair_hlpbeq.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/diving-glasses_blowwb.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/honeycomb_wucbxb.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130980/shopWesataIcon/footprint_lhzpw1.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/coral_vvpohj.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/coconut-tree_qaf8ae.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130979/shopWesataIcon/cherry_u5ima3.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/camping_maxzbe.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/balloons_fuo1xa.png',
+        'https://res.cloudinary.com/bagastri07/image/upload/v1655130978/shopWesataIcon/bee_qswc8z.png'
+    ]
+];
